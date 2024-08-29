f591fa723ca4844bc421bfa7d0d2264e4870f550
Harjot <144142200+harjot38@users.noreply.github.com>
2024-08-29 08:34:33 +0000

    Lexus is the GOAL!!

--- 01_restaurant/challenges/01_03_printmenus_ch.sql
+++ 01_restaurant/challenges/01_03_printmenus_ch.sql
@@ -3,3 +3,18 @@
 -- Create a report with all the items sorted by price (lowest to highest).
 -- Create a report showing appetizers and beverages.
 -- Create a report with all items except beverages.
+
+-- 1ST
+SELECT * 
+FROM Dishes 
+ORDER BY price
+
+-- 2ND
+SELECT * 
+FROM Dishes
+WHERE type = 'Appetizer' OR type = 'Beverage'
+
+-- 3ND
+SELECT * 
+FROM Dishes
+WHERE type != 'Beverage'
